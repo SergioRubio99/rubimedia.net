c169fb2ed81530fcda83ec2fd36805d5f62fbe8e
= <=>
2021-09-09 12:03:13 +0200

    #Logo tweaks

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-gsap.fromTo('.header_left-logoChar', { opacity: 0, x: -30 }, { opacity: 1, x: 15 })
+gsap.fromTo('.header_left-logoChar', { opacity: 0, x: -30 }, { delay: 0.5, opacity: 1, x: 15 })
     // gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { opacity: 1, x: 0, rotation: 220 })
 
 // gsap.from('.header_left-logoImg', { scaleX: 0.2, scaleY: 0.3 })
@@ -7,7 +7,7 @@ const timeline = gsap.timeline({
     // defaults: { duration: 1 }
 })
 timeline
-gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { opacity: 1, x: 0, rotation: 180 })
+gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1.5, opacity: 1, x: 0, rotation: 180 })
 gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
 
 const timelineChar = gsap.timeline({
@@ -16,4 +16,4 @@ const timelineChar = gsap.timeline({
 timelineChar
 gsap.fromTo(".header_left-logoChar", { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
 
-gsap.fromTo('.header_left-hidden', { width: 130 }, { delay: 2, width: 0 })
+gsap.fromTo('.header_left-hidden', { width: 130 }, { delay: 2, width: 0, reverse: true })
