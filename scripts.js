62c3a648f98c0d572430efad96a0735e4663c032
= <=>
2021-09-09 12:57:55 +0200

    #Background selection

--- scripts.js
+++ scripts.js
@@ -10,10 +10,6 @@ timeline
 gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1.5, opacity: 1, x: 0, rotation: 180 })
 gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
 
-const timelineChar = gsap.timeline({
-    // defaults: { duration: 1 }
-})
-timelineChar
-gsap.fromTo(".header_left-logoChar", { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
 
-gsap.fromTo('.header_left-hidden', { width: 130 }, { delay: 2, width: 0, reverse: true })
+gsap.fromTo(".header_left-logoChar", { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
+gsap.fromTo('.header_left-hidden', { x: 0 }, { ease: Power3, delay: 2.2, x: 119 })
