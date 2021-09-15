deb1a3cdcd0a377b6c8d1dbac0f3d676f91ce3f0
= <=>
2021-09-16 01:53:37 +0200

    #Responsive 1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const d = document;
 
-gsap.fromTo('.header', { y: -60 }, { ease: 'circ.out', duration: 1, delay: 0.3, y: 0 })
+if (window.innerWidth > 800) {
+    gsap.fromTo('.header', { y: -60 }, { ease: 'circ.out', duration: 1, delay: 0.3, y: 0 })
+}
+
 gsap.fromTo('.header_left-logoChar', { opacity: 0, x: -30 }, { delay: 0.5, opacity: 1, x: 15 })
     // gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { opacity: 1, x: 0, rotation: 220 })
 
@@ -10,13 +13,28 @@ const timeline = gsap.timeline({
     // defaults: { duration: 1 }
 })
 timeline
-gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1.5, opacity: 1, x: 0, rotation: 180 })
-gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
-gsap.fromTo('.header_left-logoImg', { rotation: 180 }, { delay: 1.5, rotation: 200 })
 
-gsap.fromTo(".header_left-logoChar", { scaleX: 1, scaleY: 1 }, { delay: 1.5, scaleX: 0.8, scaleY: 0.8 })
-gsap.fromTo('.header_left-hidden', { y: 1, x: 0 }, { y: 1, ease: Power3, delay: 2.5, x: 119 })
-gsap.fromTo('.layer2', { y: 0 }, { ease: Power0, duration: 1.5, y: -10 })
+
+if (window.innerWidth > 600) {
+    gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1.5, opacity: 1, x: 0, rotation: 180 })
+    gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
+    gsap.fromTo('.header_left-logoImg', { rotation: 180 }, { delay: 1.5, rotation: 200 })
+    gsap.fromTo(".header_left-logoChar", { scaleX: 1, scaleY: 1 }, { delay: 1.5, scaleX: 0.8, scaleY: 0.8 })
+    gsap.fromTo('.header_left-hidden', { y: 1, x: 0 }, { y: 1, ease: Power3, delay: 2.5, x: 119 })
+    gsap.fromTo('.layer2', { y: 0 }, { ease: Power0, duration: 1.5, y: -10 })
+} else {
+    //Responsive Mobile:
+    gsap.fromTo(".header_left-logoChar", { scaleX: 2.6, scaleY: 2.6 }, { delay: 1.5, scaleX: 2, scaleY: 2 })
+    gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1, opacity: 1, x: 0, rotation: 180 })
+    gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1.5, scaleX: 0.8, scaleY: 0.8 })
+    gsap.fromTo('.header_left-logoImg', { rotation: 180 }, { delay: 1.5, rotation: 180 })
+        //Animación barra derecha:
+    gsap.fromTo(".header_right-linkedIn", { scaleX: 1.7, scaleY: 1.7, x: 300 }, { duration: 1, x: 0, scaleX: 1.7, scaleY: 1.7 })
+    gsap.fromTo('.header_right-github', { scaleX: 1.7, scaleY: 1.7 }, { scaleX: 1.7, scaleY: 1.7 })
+    gsap.fromTo('.header_right-contact ', { scaleX: 1.7, scaleY: 1.7 }, { scaleX: 1.7, scaleY: 1.7 })
+
+}
+
 
 
 
