b12fb7bb91a4a3cff57b74c05b57c4a96f75d081
= <=>
2021-09-18 18:58:15 +0200

    #New Desktop Design

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
 const d = document;
 
-if (window.innerWidth > 800) {
-    gsap.fromTo('.header', { y: -60 }, { ease: 'circ.out', duration: 1, delay: 0.3, y: 0 })
-}
-
 gsap.fromTo('.header_left-logoChar', { opacity: 0, x: -30 }, { delay: 0.5, opacity: 1, x: 15 })
     // gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { opacity: 1, x: 0, rotation: 220 })
 
@@ -16,24 +12,22 @@ timeline
 
 
 if (window.innerWidth > 600) {
-    gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1.5, opacity: 1, x: 0, rotation: 180 })
-    gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1, scaleX: 0.8, scaleY: 0.8 })
-    gsap.fromTo('.header_left-logoImg', { rotation: 180 }, { delay: 1.5, rotation: 200 })
-    gsap.fromTo(".header_left-logoChar", { scaleX: 1, scaleY: 1 }, { delay: 1.5, scaleX: 0.8, scaleY: 0.8 })
-    gsap.fromTo('.header_left-hidden', { y: 1, x: 0 }, { y: 1, ease: Power3, delay: 2.5, x: 119 })
+    gsap.fromTo('.header', { y: -90 }, { ease: 'circ.out', duration: 1, delay: 0.3, y: 0 })
+
+    gsap.fromTo('.header_left-logoImg', { y: -100 }, {  y: 0 })
+    gsap.fromTo('.header_left-logoImg', { scaleX: 1.3, scaleY: 1.3 }, { delay: 1, scaleX: 1.3, scaleY: 1.3 })
+    gsap.fromTo(".header_left-logoChar", { scaleX: 1.2, scaleY: 1.2 }, { delay: 1.5, scaleX: 1, scaleY: 1 })
     gsap.fromTo('.layer2', { y: 0 }, { ease: Power0, duration: 1.5, y: -10 })
 
 
-    gsap.fromTo('.presentation-btn', { y: 0, opacity: 0, }, { duration: 1, y: 8, delay: 1, duration: 1, opacity: 1, });
-    gsap.fromTo('.presentation-title', { y: -10, opacity: 0, }, { y: 0, delay: 1, duration: 1, opacity: 1, });
-    gsap.fromTo('.presentation-number', { y: -10, opacity: 0, }, { y: 0, delay: 1, duration: 1, opacity: 1, });
+    // gsap.fromTo('.presentation-btn', { y: 0, opacity: 0, }, { duration: 1, y: 8, delay: 1, duration: 1, opacity: 1, });
+    // gsap.fromTo('.presentation-title', { y: -10, opacity: 0, }, { y: 0, delay: 1, duration: 1, opacity: 1, });
 
 } else {
     //Responsive Mobile:
-    gsap.fromTo(".header_left-logoChar", { scaleX: 2.6, scaleY: 2.6 }, { delay: 1.5, scaleX: 2, scaleY: 2 })
-    gsap.fromTo('.header_left-logoImg', { opacity: 0, x: -20 }, { delay: 1, opacity: 1, x: 0, rotation: 180 })
-    gsap.fromTo('.header_left-logoImg', { scaleX: 1, scaleY: 1 }, { delay: 1.5, scaleX: 0.8, scaleY: 0.8 })
-    gsap.fromTo('.header_left-logoImg', { rotation: 180 }, { delay: 1.5, rotation: 180 })
+    gsap.fromTo('.header_left-logoImg', { y: -100 }, {  y: 0 })
+    gsap.fromTo(".header_left-logoChar", { scaleX: 1.2, scaleY: 1.2 }, { delay: 1.5, scaleX: 1, scaleY: 1 })
+    gsap.fromTo('.layer2', { y: 0 }, { ease: Power0, duration: 1.5, y: -10 })
         //Animación barra derecha:
     gsap.fromTo(".header_right-linkedIn", { opacity: 0 }, { delay: 2.5, opacity: 1, duration: 2 })
     gsap.fromTo('.header_right-github', { opacity: 0 }, { delay: 2.5, opacity: 1, duration: 2 })
@@ -41,7 +35,6 @@ if (window.innerWidth > 600) {
 
     gsap.fromTo('.presentation-btn', { y: 0, opacity: 0, }, { duration: 1, delay: 2.6, duration: 1, opacity: 2, });
     gsap.fromTo('.presentation-title', { opacity: 0, }, { delay: 2.6, duration: 2, opacity: 1, });
-    gsap.fromTo('.presentation-number', { opacity: 0, }, { delay: 2.6, duration: 2, opacity: 1, });
 
 }
 
@@ -119,7 +112,7 @@ if (window.innerWidth > 600) {
         }, 1);
 
         setInterval(() => {
-            if (a > 1200) {
+            if (a > 1080) {
                 clearInterval(int);
             }
         })
